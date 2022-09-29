dd272882c44b76e6c0a908bb9c74d47130e4d443
isberg1 <isberg1_github@outlook.com>
2022-09-29 20:27:53 +0200

    improved build script

--- utils/build.js
+++ utils/build.js
@@ -1,5 +1,7 @@
+// Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = "production";
 process.env.NODE_ENV = "production";
+process.env.ASSET_PATH = "/";
 
 var webpack = require("webpack"),
   config = require("../webpack.config");
@@ -13,9 +15,20 @@ if (arg.includes("--watch")) {
 
 webpack(config, function (err, rest) {
   if (err) throw err;
+  const date = new Date();
 
-  console.log("");
-  console.log("--------------------------------------");
+  console.clear();
+  console.log("---------------------------------------");
+
+  if (rest?.compilation?.warnings.length) {
+    console.log("warnings:", rest.compilation.warnings);
+  }
+
+  if (rest?.compilation?.errors.length) {
+    console.log("warnings:", rest.compilation.errors);
+  }
+
   console.log("\tBuilding", config.watch ? " --watch" : "");
-  console.log("--------------------------------------");
+  console.log(`\t${date.toLocaleString()}`);
+  console.log("---------------------------------------");
 });
